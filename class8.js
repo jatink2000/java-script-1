405fbc9b2414db12396e245f23ad5fc616f91151
jatink2000 <jatink8059@gmail.com>
2026-08-03 13:57:25 +0530

    sj

---
+++ class8.js
@@ -0,0 +1,85 @@
+
+
+// string : string is used to stote the text . 
+
+    // how to store the string in a variable : to store the string , declare a variable and then store the text inside the single and double quotes . 
+
+    // let mystring='groot academy'
+    // let mystring2="groot academy"
+
+    // document.write(mystring ,"<br/>")
+    // document.write(mystring2)
+
+
+
+    // how to access the string elements : to access the string elements by using the index number and the string index start with 0 . 
+    // let a="hello js"
+    // document.write(a[0], "<br/>") // h 
+    // document.write(a[1], "<br/>") // e
+    // document.write(a[2]) // l  
+
+
+    
+    // special char :  
+    // let a="my name is \"groot academy.\" "
+    // document.write(a)
+
+    // let a='it\'s groot'
+    // document.write(a) 
+
+    // let a="my name is groot academy\\ groot software"
+    // document.write(a)
+
+
+    // let a=`Hello
+    //         groot 
+    //         academy`
+        
+    // document.write(a)
+
+
+
+    // let year=2026 
+    // let name="ravi"
+
+    // let mystring=`my name is ${name}. and year is ${year}`
+    // document.write(mystring)
+
+
+
+    // let a=prompt("enter first name:")
+    // let b=prompt("enter last name:")
+    // let mystring=`your full name is ${a+b}.`
+    // document.write(mystring)
+
+
+    // string function : 
+    // 1. length: the length is used to get/access the number of length in a string . 
+    // let a="groot academy."
+    // let b=a.length 
+    // document.write("string length is: ",b)
+
+
+
+    // 2. at() : the at function is used to access the string element by index number .  
+    // let a="groot academy."
+    // let b=a.at(4) 
+    // document.write(b)
+
+
+    // 3. charAt() : the charAt function is used to access the string element by index number .  
+    // let a="groot academy."
+    // let b=a.charAt(2) 
+    // document.write(b)
+
+
+    // 4. slice() : used to select the string elements . 
+        // slice(start,end) // end not count
+
+    // let a="groot academy."
+    // let b=a.slice(1,8)  // 1,7 
+    // document.write(b)
+
+
+    // 5. substring : similar to slice function . 
+
