0ba1b5904e4c6c74cdb7d0216d29eab95361baeb
jatink2000 <jatink8059@gmail.com>
2026-08-06 13:54:32 +0530

    sj

--- class9.js
+++ class9.js
@@ -16,8 +16,114 @@
 
 
     // change / modify the arrays element : 
-    let student=[78,67,89,67,89,87,98,67,87,55,67]
-    student[3]=90
-    student[0]=45
-    document.write(student)
+    // let student=[78,67,89,67,89,87,98,67,87,55,67]
+    // student[3]=90
+    // student[0]=45
+    // document.write(student)
+
+
+
+    // array methods : 
+    // 1. length: the length method is used to get the length of our array element . 
+
+    // let student=[78,67,89,67,89,87,98,67,87,55,67]
+    // let b=student.length 
+    // document.write(b) 
+
+
+    // 2. pop() : the pop() method is used to remove the last data in an array . 
+
+    // let a=[56,67,78,89,98,76,87]
+    // let b=a.pop() 
+    // document.write(a)
+
+    // 3. push() : the push method is used to add the data at last in an array . 
+
+    // let a=[56,67,78,89,98,76,87]
+    // let b=a.push(90)
+    // document.write(a)
+
+
+    // 4. shift() : the shift method is used to remove the data at first in an array . 
+    // let a=[56,67,78,89,98,76,87]
+    // let b=a.shift()
+    // document.write(a)
+
+    // 5. unshift(): the unshift method is used to add the array element at first . 
+
+    // let a=[56,67,78,89,98,76,87]
+    // let b=a.unshift(70)
+    // document.write(a)
+
+    // 6. join() : the join method is used to join the array element . 
+
+    // let a=[56,67,78,89,98,76,87]
+    // let b=a.join("_")
+    // document.write(b)
+
+
+
+    // 7. at() : the at() method is used to get the array element by index number . 
+
+    // let a=[56,67,78,89,98,76,87]
+    // let b=a.at(3) // 89 
+    // document.write(b)
+
+
+    // 8. slice() the slice() method is used to select the array element . 
+        // slice(start index, end index) 
+            // endindex not count . 
+
+        // let a=[56,67,78,89,98,76,87]
+        // let b=a.slice(1,5) // 0,4 
+        // document.write(b) 
+
+    // 9. splice(): splice() method is used to add and remove the data at anywhere in an array .
+        // splice(target index,deletecount,newdata....)
+        
+    // let a=[56,67,78,89,98,76,87]
+    // let b=a.splice(3,0,345,678)
+    // document.write(a)
+
+
+    // 10. copywithin() : the copywithin method is used to copy the array element and insert it into an array . 
+        // copywithin(targetindex,copyindex)
+
+        // let a=[56,67,78,89,98,76,87]
+        // let b=a.copyWithin(3,0) // 56,67,78,56,67,78,89
+        // document.write(b)
+
+
+    // 11. concat() : the concat method is used to marge the two array and return a new array . 
+
+    // let a=[56,67,78,89,98,76,87]
+    // let b=[123,456,789]
+    // let c=b.concat(a) // 123,456,789,56,67,78,89,98,76,87
+    // document.write(c)
+
+
+    // 12. indexof(): the indexod method is used to get the index number of given element . 
+    // let a=[123,456,789]
+    // let b=a.indexOf(123)
+    // document.write(b)
+
+    // 13. includes : check element peresent or not . 
+
+    // let a=[123,456,789]
+    // let b=a.includes(124)
+    // document.write(b)
+
+
+
+    // object , json 
+    // map() 
+    // filter() 
+    // reduce() 
+
+
+        
+
+
+
+
 
