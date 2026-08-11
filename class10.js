0ad7b599870ffdad78d1d4bdf8518017390721dc
jatink2000 <jatink8059@gmail.com>
2026-08-11 13:52:23 +0530

    sj

--- class10.js
+++ class10.js
@@ -5,124 +5,191 @@
 
 
 
-    // How to create/declare the object in java script :to create the object in java script use the object name and then store the multiple key and value data inside the {} . 
+// How to create/declare the object in java script :to create the object in java script use the object name and then store the multiple key and value data inside the {} . 
 
-    // let objectname={
-    //     key:value ,
-    //     key2:value2 , 
-    //     key3: value3 
-    //     ...
-    // }
+// let objectname={
+//     key:value ,
+//     key2:value2 , 
+//     key3: value3 
+//     ...
+// }
 
+// let student_details={
+//     studentname:"Ravi kumar",
+//     marks:87 ,
+//     course: 'C & C++'
+// }
+
+// key : properties 
+// value/data : property value 
+
+// how to access the object properties : 
+// 1. using dot: 
+// objectname.propertyname
+// document.write(student_details.marks , "<br/>")
+// document.write(student_details.course, "<br/>")
+
+
+
+// 2. using property name  / bracket notation: 
+// objectname[propertyname]
+
+// document.write(student_details["studentname"], "<br/>")
+// document.write(student_details["marks"])
+
+
+
+
+// another way to create the object : you can declare the empty object and add the properties later . 
+
+// let student_details={} // empty object 
+// let student_details2={} // empty object 
+
+//add properties
+// objectname.propertyname=data/value 
+
+// student_details.firstname="groot"
+// student_details.lastname="academy"
+
+// student_details2.marks=60 
+// student_details2.age=26
+
+
+// document.write(student_details.firstname, "<br/>")
+// document.write(student_details2.age, "<br/>")
+
+
+// using the new keyword : 
+
+// let student_details=new Object({
+//     firstname:"groot",
+//     lastname:"academy"
+// })
+
+
+// there is no need to use the new Object() . 
+
+
+
+// change the properties value / data : you can change the value of property .
+
+// objectname.propertyname=newdata 
+
+// let student_details={
+//         firstname:"groot",
+//         lastname:"academy"
+// }
+
+// student_details.firstname="ravi"
+// student_details.lastname="kumar"
+
+// document.write(student_details)
+
+
+
+// add new property : you can add a new property .
+// objectname.newpropertyname=data 
+
+// let student_details={
+//         firstname:"groot",
+//         lastname:"academy"
+// }
+
+// student_details.course="Web designing"
+
+// console.log(student_details)
+
+
+
+// delete property : the delete keyword is used to delete the property . 
+// delete objectname.properyname
+
+// let student_details={
+//         firstname:"groot",
+//         lastname:"academy",
+//         course:"Web designing"
+// }
+// delete student_details.firstname 
+// console.log(student_details)
+
+
+// check : 
+
+// let student_details={
+//         firstname:"groot",
+//         lastname:"academy",
+//         course:"Web designing"
+// }
+
+// let result=("age" in student_details)
+// console.log(result)
+
+
+
+
+// let student1={
+//         firstname:"ravi",
+//         lastname:"kumar",
+//         course:"Web designing"
+// }
+
+// let student2={
+//         firstname:"ankit",
+//         lastname:"yadav",
+//         course:"Web development"
+// }
+
+// let student3={
+//         firstname:"ravi",
+//         lastname:"kumar",
+//         course:"Web designing"
+// }
+
+
+
+// json : java script oriented notation 
+// with the help of json we can store the multiple objects into a single variable . 
+
+// let json_name = [
+//     {
+//         firstname: "ravi",
+//         lastname: "kumar",
+//         course: "Web designing"
+//     },
+//     {
+//         firstname: "ravi",
+//         lastname: "kumar",
+//         course: "Web designing"
+//     },
+//     . . . . . . . .
+// ]
+
+
+// let obj1={a:1,b:2}
+// let obj2={...obj1,c:3}
+// let obj3=[{...obj1},{...obj2}]
+// console.log(obj2) // 3 
+// console.log(obj3) //{a:1,b:2,c:3,a:1,b:2,d:4}
+
+
+
+    // nested object : 
     // let student_details={
-    //     studentname:"Ravi kumar",
-    //     marks:87 ,
-    //     course: 'C & C++'
+    //     information:{
+    //         name:"jatin",
+    //         course:"Web"
+    //     },
+    //     address:{
+    //         city:"jaipur",
+    //         pincode:"13456",
+    //         street:{
+    //             address1:"gurjar ki thadi",
+    //             address2:"mahi path"
+    //         }
+    //     }
     // }
 
-    // key : properties 
-    // value/data : property value 
-
-    // how to access the object properties : 
-            // 1. using dot: 
-                // objectname.propertyname
-                // document.write(student_details.marks , "<br/>")
-                // document.write(student_details.course, "<br/>")
-
-
-
-            // 2. using property name  / bracket notation: 
-                // objectname[propertyname]
-
-                // document.write(student_details["studentname"], "<br/>")
-                // document.write(student_details["marks"])
-
-
-
-    
-    // another way to create the object : you can declare the empty object and add the properties later . 
-
-        // let student_details={} // empty object 
-        // let student_details2={} // empty object 
-
-        //add properties
-        // objectname.propertyname=data/value 
-
-        // student_details.firstname="groot"
-        // student_details.lastname="academy"
-
-        // student_details2.marks=60 
-        // student_details2.age=26
-
-
-        // document.write(student_details.firstname, "<br/>")
-        // document.write(student_details2.age, "<br/>")
-
-
-    // using the new keyword : 
-
-        // let student_details=new Object({
-        //     firstname:"groot",
-        //     lastname:"academy"
-        // })
-
-
-        // there is no need to use the new Object() . 
-
-
-
-    // change the properties value / data : you can change the value of property .
-        
-        // objectname.propertyname=newdata 
-
-    // let student_details={
-    //         firstname:"groot",
-    //         lastname:"academy"
-    // }
-
-    // student_details.firstname="ravi"
-    // student_details.lastname="kumar"
-
-    // document.write(student_details)
-
-
-
-    // add new property : you can add a new property .
-        // objectname.newpropertyname=data 
-    
-    // let student_details={
-    //         firstname:"groot",
-    //         lastname:"academy"
-    // }
-
-    // student_details.course="Web designing"
-
-    // console.log(student_details)
-
-
-
-    // delete property : the delete keyword is used to delete the property . 
-        // delete objectname.properyname
-
-    // let student_details={
-    //         firstname:"groot",
-    //         lastname:"academy",
-    //         course:"Web designing"
-    // }
-    // delete student_details.firstname 
-    // console.log(student_details)
-
-
-    // check : 
-
-    let student_details={
-            firstname:"groot",
-            lastname:"academy",
-            course:"Web designing"
-    }
-
-    let result=("age" in student_details)
-    console.log(result)
+    // console.log(student_details.address.street.address1)
 
 
 
@@ -132,7 +199,8 @@
 
 
 
-    
+
+
 
 
 
---
+++ class11.js
@@ -0,0 +1,62 @@
+
+
+// students : 
+
+let myallstudents=[
+    {
+        students_name:"Ravi",
+        coursedetails:{
+            course_name:"Web Development",
+            course_duration:"5-6 Months",
+            course_fee:25000       
+        },
+        email:"ravi@gmal.com",
+        parent_information:{
+            parents_name:"xyz",
+            parents_email:"xyz@gmail.com",
+            contact:9876543211
+        }
+    },
+    {
+        students_name:"Ravi",
+        coursedetails:{
+            course_name:"Web Development",
+            course_duration:"5-6 Months",
+            course_fee:25000       
+        },
+        email:"ravi@gmal.com",
+        parent_information:{
+            parents_name:"xyz",
+            parents_email:"xyz@gmail.com",
+            contact:9876543211
+        }
+    },
+    {
+        students_name:"Ravi",
+        coursedetails:{
+            course_name:"Web Development",
+            course_duration:"5-6 Months",
+            course_fee:25000       
+        },
+        email:"ravi@gmal.com",
+        parent_information:{
+            parents_name:"xyz",
+            parents_email:"xyz@gmail.com",
+            contact:9876543211
+        }
+    },
+    {
+        students_name:"Ravi",
+        coursedetails:{
+            course_name:"Web Development",
+            course_duration:"5-6 Months",
+            course_fee:25000       
+        },
+        email:"ravi@gmal.com",
+        parent_information:{
+            parents_name:"xyz",
+            parents_email:"xyz@gmail.com",
+            contact:9876543211
+        }
+    }
+]
