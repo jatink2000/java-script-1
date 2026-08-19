53e4f0a33ef4a785608fa96de3e5ba77c4dd9894
jatink2000 <jatink8059@gmail.com>
2026-08-19 13:52:59 +0530

    s

--- class13.js
+++ class13.js
@@ -46,3 +46,55 @@
         let e= document.getElementsByTagName("p")
         e[0].innerHTML="this is my updated paragraph"
         e[3].innerHTML="this is my second updated paragraph"
+
+
+
+    // how to style the html elements using dom : 
+
+    // 1. by id : 
+        document.getElementById("groot").style.color="red"
+        document.getElementById("groot1").style.backgroundColor="blue"
+
+
+    // 2. byclassname : 
+        let ab=document.getElementsByClassName("groot1")
+        ab[0].style.color="yellow"
+        ab[1].style.backgroundColor="blue"
+
+
+    // 3. by tagname : 
+       let cd= document.getElementsByTagName("p")
+       cd[1].style.fontSize="40px"
+       cd[1].style.backgroundColor="black"
+       cd[1].style.color="blue"
+
+
+    // how to create a element by using dom : 
+       let myheading= document.createElement("h2") // create h2 tag 
+       myheading.innerHTML="this is my dom heading 2" // add the text into h2 tag 
+
+       let myheading5= document.createElement("h5") // create h5 tag 
+        myheading5.innerHTML="this is my dom heading 5" 
+
+       
+       let mainbox= document.getElementById("box1")
+       mainbox.appendChild(myheading)
+       mainbox.appendChild(myheading5)
+
+
+    
+    // how to change the image by using dom : 
+      let image=  document.getElementById("myimage")
+      image.src="https://www.equinetmedia.com/hubfs/How-to-find-b2b-blog-images.png"
+
+
+
+    let myimage=document.createElement("img")
+    myimage.src="https://static.vecteezy.com/system/resources/thumbnails/060/843/811/small/close-up-of-raindrops-on-leaves-hd-background-luxury-hd-wallpaper-image-trendy-background-illustration-free-photo.jpg"
+
+    let xy=document.getElementById("mynewimage")
+    xy.appendChild(myimage)
+
+
+    
+
